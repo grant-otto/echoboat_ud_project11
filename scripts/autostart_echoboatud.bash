37e7b2f99866be52f848b229d051b3eaef69c597
grant-otto <gotto@udel.edu>
2022-03-11 17:16:26 +0100

    finalized crontab

--- scripts/autostart_echoboatud.bash
+++ scripts/autostart_echoboatud.bash
@@ -5,7 +5,7 @@
 
 DAY=$(date "+%Y-%m-%d")
 NOW=$(date "+%Y-%m-%dT%H.%M.%S.%N")
-LOGDIR="/home/field/project11/log/${DAY}"
+LOGDIR="/home/jetson/project11/log/${DAY}"
 mkdir -p "$LOGDIR"
 LOG_FILE="${LOGDIR}/autostart_${NOW}.txt"
 
@@ -24,19 +24,14 @@ source /home/jetson/project11/catkin_ws/devel/setup.bash
 
 set -v
 
-#export ROS_WORKSPACE=/home/jetson/project11/catkin_ws
+export ROS_WORKSPACE=/home/jetson/project11/catkin_ws
 #export ROS_IP=192.168.100.112
-
-#wait for mystique to be pingable by self
-#while ! ping -c 1 -W 1 mystique; do
-#    echo "Waiting for ping to mystique..."
-#    sleep 1
-#done
 
 echo "Wait 10 seconds before launching ROS..."
 sleep 10
 
-
 /usr/bin/tmux new -d -s project11 roscore
 /usr/bin/tmux splitw -p 90
 /usr/bin/tmux send-keys "roslaunch echoboat_ud_project11 echo_robot.launch" C-m
+
+} >> "${LOG_FILE}" 2>&1
